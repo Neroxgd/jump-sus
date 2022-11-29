ff9512a19fc00c904ab98fe502235d6fa913b557
Neroxgd <darkaux69@gmail.com>
2022-11-29 09:14:41 +0100

    je

--- Assets/Scripts/JambeWalk.cs
+++ Assets/Scripts/JambeWalk.cs
@@ -9,17 +9,18 @@ public class JambeWalk : MonoBehaviour
     [SerializeField] Transform Lleg;
     [SerializeField] private float leglerp = 0.5f;
     private float speedWalk;
-    public float ReturnSpeedWalk(){return speedWalk;}
+    public float ReturnSpeedWalk() { return speedWalk; }
     private bool directionwalk = true;
     [SerializeField] private float SetSpeedWalk;
 
     void Update()
     {
+
         //move legs
         if (_playerController.DeltaLookMoveReturn() != Vector2.zero)
         {
-            Rleg.localRotation = Quaternion.Lerp(Quaternion.Euler(-45, 0, 0), Quaternion.Euler(45, 0, 0), leglerp);
-            Lleg.localRotation = Quaternion.Lerp(Quaternion.Euler(-45, 0, 0), Quaternion.Euler(45, 0, 0), Mathf.Abs(leglerp - 1));
+            Rleg.localRotation = Quaternion.Slerp(Quaternion.Euler(-45, 0, 0), Quaternion.Euler(45, 0, 0), leglerp);
+            Lleg.localRotation = Quaternion.Slerp(Quaternion.Euler(-45, 0, 0), Quaternion.Euler(45, 0, 0), Mathf.Abs(leglerp - 1));
 
             if (_playerController.DeltaLookMoveReturn().x > _playerController.DeltaLookMoveReturn().y)
                 speedWalk = Mathf.Abs(_playerController.DeltaLookMoveReturn().x / SetSpeedWalk);
@@ -30,7 +31,7 @@ public class JambeWalk : MonoBehaviour
                 directionwalk = !directionwalk;
             else if (leglerp >= .99f)
                 directionwalk = !directionwalk;
-            
+
 
             // if (directionwalk)
             //     speedWalk = -Mathf.Abs(speedWalk);
@@ -41,8 +42,9 @@ public class JambeWalk : MonoBehaviour
 
             leglerp += speedWalk * Time.deltaTime;
             leglerp = Mathf.Clamp(leglerp, 0, 1);
-            if (speedWalk > -.05 && speedWalk< .05)
+            if (speedWalk > -.05 && speedWalk < .05)
                 leglerp = 0.5f;
         }
+
     }
 }
